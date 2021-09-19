35fb16962c0a8369b48b49f2ce88272a8eb94a5d
abrshdir <abrhamdiriba.ad@gmail.com>
2021-09-19 13:42:41 +0300

    erros fixed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,46 @@
 <template>
-    <!--    Caption on the very first top of the page-->
-    <div class="caption">
-        <font-awesome-icon icon="exclamation-triangle"
-                           style=" color: #FC732F; width: 24px; height: 24px; line-height: 40px; font-weight: 800; padding-right: 16px"/>
-        <span>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</span>
-    </div>
+    <div>
+        <!--    Caption on the very first top of the page-->
+        <div class="caption">
+            <font-awesome-icon icon="exclamation-triangle"
+                               style=" color: #FC732F; width: 24px; height: 24px; line-height: 40px; font-weight: 800; padding-right: 16px"/>
+            <span>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</span>
+        </div>
 
-    <!--    Header the second top layer after caption that holds the navigation links aa well as logo and icons for smaller devices-->
-    <div class="header">
-        <div class="bars" @click="ToggleDrawer">
-            <!--            np install fort awesome and related brand icons-->
-            <div v-if="drawerOpened">
-                <font-awesome-icon :icon="['fa', 'times']"
-                                   style=" color: #FFFFFF; line-height: 40px; font-weight: 800"/>
+        <!--    Header the second top layer after caption that holds the navigation links aa well as logo and icons for smaller devices-->
+        <div class="header">
+            <div class="bars" @click="ToggleDrawer">
+                <!--            np install fort awesome and related brand icons-->
+                <div v-if="drawerOpened">
+                    <font-awesome-icon :icon="['fa', 'times']"
+                                       style=" color: #FFFFFF; line-height: 40px; font-weight: 800"/>
+                </div>
+                <div v-else-if="!drawerOpened">
+                    <font-awesome-icon :icon="['fa', 'bars']"
+                                       style=" color: #FFFFFF; line-height: 40px; font-weight: 800"/>
+                </div>
             </div>
-            <div v-else-if="!drawerOpened">
-                <font-awesome-icon :icon="['fa', 'bars']" style=" color: #FFFFFF; line-height: 40px; font-weight: 800"/>
+
+            <h1 className="main-navigation__title logo">
+                <font-awesome-icon :icon="['fab', 'rocketchat']"
+                                   style=" color: #FC732F; line-height: 40px; font-weight: 800; padding-right: 16px"/>
+                <span style="color: #FC732F"><span style="color: #FFFFFF">Logo</span>Ipsum</span>
+            </h1>
+
+            <nav class="mobileMenu" :style="{display: drawerOpened ? 'block': 'none'}">
+                <NavLinks :logout="false"/>
+            </nav>
+
+            <nav class="desktopMenu">
+                <NavLinks :logout="!drawerOpened"/>
+            </nav>
+
+            <div class="logout">
+                <font-awesome-icon :icon="['fa', 'sign-out-alt']"
+                                   style=" color: #FFFFFF; line-height: 40px; font-weight: 800;"/>
             </div>
         </div>
 
-        <h1 className="main-navigation__title logo">
-            <font-awesome-icon :icon="['fab', 'rocketchat']"
-                               style=" color: #FC732F; line-height: 40px; font-weight: 800; padding-right: 16px"/>
-            <span style="color: #FC732F"><span style="color: #FFFFFF">Logo</span>Ipsum</span>
-        </h1>
-
-        <nav class="mobileMenu" :style="{display: drawerOpened ? 'block': 'none'}">
-            <NavLinks :logout="false"/>
-        </nav>
-
-        <nav class="desktopMenu">
-            <NavLinks :logout="!drawerOpened"/>
-        </nav>
-
-        <div class="logout">
-            <font-awesome-icon :icon="['fa', 'sign-out-alt']"
-                               style=" color: #FFFFFF; line-height: 40px; font-weight: 800;"/>
-        </div>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,41 +1,29 @@
-// const { createApp } = require('vue');
-// import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faRocketchat } from '@fortawesome/free-brands-svg-icons'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import Vue from 'vue'
-import App from './App.vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faRocketchat } from '@fortawesome/free-brands-svg-icons';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import router from "./router";
+import Dialog from "./components/Dialog";
+const app = createApp(App);
 
-// import router from './router';
-import Dialog from './components/Dialog';
-// const app = createApp(App);
-
-library.add(faRocketchat);
-library.add(faSignOutAlt);
-library.add(faTimes);
-library.add(faCircle);
-library.add(faExchangeAlt);
-library.add(faExclamationTriangle);
-library.add(faCheckCircle);
-library.add(faBars);
-// app.component('base-dialog', Dialog);
-// app.component('font-awesome-icon', FontAwesomeIcon);
-// app.use(router);
-// app.mount('#app');
-// Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
-// createApp(App).mount("#app");
+library.add(faRocketchat)
+library.add(faSignOutAlt)
+library.add(faTimes)
+library.add(faCircle)
+library.add(faExchangeAlt)
+library.add(faExclamationTriangle)
+library.add(faCheckCircle)
+library.add(faBars)
+app.component('base-dialog', Dialog)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+app.mount('#app')
